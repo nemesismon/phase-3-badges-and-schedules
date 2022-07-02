6c01013c55f582b831858e63c5217eb3b1e83854
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-07-02 13:40:24 -0400

    commit tree 6fe8d97a732a97a85b43b35a35911361962019a8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator (names)
+    return badge = names.map{ |name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms (names)
+    return room = names.map.with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer (attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendees).each do |assign|
+        puts assign
+    end
+end
